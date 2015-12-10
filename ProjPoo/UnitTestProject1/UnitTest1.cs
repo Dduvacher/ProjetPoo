c197b040c174f39f52c3cba99aa047fc5a054daf
Petwag <florentin.hortet@gmail.com>
2015-12-10 19:52:55 +0100

    debut test

---
+++ ProjPoo/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjPoo;
+using System.Collections.Generic;
+
+
+
+namespace UnitTest_ProjPoo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNomJoueur()
+        {
+            GameBuilder g = new GameBuilder();
+            GameImpl Game = g.createGame();
+            g.createMap(0, Game);
+            g.addPlayer1("Hortet", "Florentin", FactoryRace.INSTANCE.getOrc());
+            g.addPlayer2("Duvacher", "Damien", FactoryRace.INSTANCE.getElf());
+
+            Assert.AreEqual("Hortet",g.)
+        }
+    }
+}
