9c7a0e6136d837a83384d4988f968de3d97adae2
Petwag <florentin.hortet@gmail.com>
2016-01-07 09:16:24 +0100

    WPF

---
+++ ProjPoo/WpfApplication1/Image.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ProjPoo;
+
+namespace WpfApplication1
+{
+    class Image
+    {
+        private ImageBrush mountain = null;
+        private ImageBrush plain = null;
+        private ImageBrush forest = null;
+        private ImageBrush water = null;
+
+        private Image()
+        {
+            water = new ImageBrush(new BitmapImage(new Uri(@"Image/water.png", UriKind.Relative)));
+        }
+
+        public Brush BrushTile(Tiles a)
+        {
+            ImageBrush brush = null;
+            if (a is WaterImpl)
+            {
+                brush = water;
+            }
+            return brush;
+        }
+    }
+
+    
+}
